9bd9661497770f56075f609159523fe8c374b2b4
Nathan <natms24@gmail.com>
2023-10-13 21:21:06 +0300

    Basic Game Functionality Finished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const buttonEl = document.getElementById('start')
+
 // playfield
 let board
 const boardWidth = 360
@@ -9,7 +11,8 @@ const birdWidth = 34
 const birdHeight = 24
 const birdX = boardWidth / 8
 const birdY = boardHeight / 2
-let birdImg
+let birdImg = new Image()
+birdImg.src = "../img/flappybird.png"
 let bird = {
     x: birdX,
     y: birdY,
@@ -25,57 +28,87 @@ let pipeX = boardWidth
 let pipeY = 0
 let topPipeImg
 let bottomPipeImg
+topPipeImg = new Image()
+topPipeImg.src = "../img/toppipe.png"
+bottomPipeImg = new Image()
+bottomPipeImg.src = "../img/bottompipe.png"
 
 // game physics
 let velocityX = -2
+let velocityY = 0
+let gravity = 0.2
 
+let gameOver = false
+let score = 0
 
-window.addEventListener('load', function() {
+function game() {
     board = document.getElementById('board')
     board.height = boardHeight
     board.width = boardWidth
     context = board.getContext('2d') // drawing on page 
-
-    // load image
-    birdImg = new Image()
-    birdImg.src = "../img/flappybird.png"
+    
     birdImg.addEventListener('load', function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
     })
 
-    topPipeImg = new Image()
-    topPipeImg.src = "../img/toppipe.png"
-
-    bottomPipeImg = new Image()
-    bottomPipeImg.src = "../img/bottompipe.png"
-
     requestAnimationFrame(update)
     setInterval(placePipes, 1500)
-})
+    this.document.addEventListener('keydown', moveBird)
+}
+
+buttonEl.addEventListener('click', game)
 
 function update() {
     requestAnimationFrame(update)
+    if (gameOver) {
+        return
+    }
     context.clearRect(0,0, board.width, board.height)
 
     // bird
+    velocityY += gravity
+    bird.y = Math.max(bird.y + velocityY, 0)
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
     
-    // pipes.forEach((pipe) => {
-    //     pipe.x += velocityX
-    //     context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
-    // })
+    if (bird.y > board.height) {
+        gameOver = true
+    }
 
     // pipes
     for (let i = 0; i < pipes.length; i++) {
         let pipe = pipes[i]
         pipe.x += velocityX
-        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
+        context.drawImage( pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
+
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+            score += 1
+            pipe.passed = true
+        }
+        if (detectCollision(bird, pipe)) {
+            gameOver = true
+        }
     }
+
+    while (pipes && pipes[0].x < -pipeWidth) {
+        pipes.shift()
+    }
+
+    context.fillStyle = "white"
+    context.font = "45px sans-serif"
+    context.fillText(score, 5, 45)
+
+    if (gameOver) {
+        context.fillText("GAME OVER",5,90)
+    }
+
 }
 
 function placePipes() {
-    
-    let randomPipeY = pipeY - pipeHeight/4
+    if (gameOver) {
+        return
+    }
+    let randomPipeY =  - Math.floor((Math.random() * pipeHeight/2)) - pipeHeight/4
+    let gap = board.height / 4
 
     let topPipe = {
         img: topPipeImg,
@@ -85,5 +118,25 @@ function placePipes() {
         height: pipeHeight,
         passed: false
     }
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + gap,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: true
+    }
+
     pipes.push(topPipe)
+    pipes.push(bottomPipe)
+}
+
+function moveBird(e) {
+    if (e.code === "Space" || e.code === "ArrowUp" || e.code === "KeyX") {
+        velocityY = -6
+    }
+}
+
+function detectCollision(a,b) {
+    return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y
 }
